32c99690cdf8ea0cd2048a00b0857bbb846e7cac
Maruf Hossain <hossainmaruf001@gmail.com>
2023-10-03 01:53:44 +0200

    created a dragon news webpage and added header section

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC2n7agWzkJPNznNoIFoZzv72U1TnY90E4",
+  authDomain: "react-dragon-news-auth-2eddb.firebaseapp.com",
+  projectId: "react-dragon-news-auth-2eddb",
+  storageBucket: "react-dragon-news-auth-2eddb.appspot.com",
+  messagingSenderId: "504946818050",
+  appId: "1:504946818050:web:2657209fe9dc8f627f3b1b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
